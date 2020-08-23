d5fb20eb81822d2c9669aecd04d145d31399198e
Santiago <santiagouberti@gmail.com>
2020-08-23 20:56:00 -0300

    Sumé Parsel y exporto Pet a nuevo archivo

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,34 +1,26 @@
-const Pet = ({ name, animal, breed}) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed),
-    ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-    return React.createElement(
-        "div",
-        { id: "algo-importante"},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, { 
-                name: "Fortunato",
-                animal: "Tortuga",
-                breed: "Terrestre" 
-            }),
-            React.createElement(Pet, { 
-                name: "Alaska",
-                animal: "Perro",
-                breed: "Mixto" 
-            }),
-            React.createElement(Pet, { 
-                name: "Crackovia", 
-                animal: "Perro", 
-                breed: "Dálmata" 
-            })
-        ]
-    );
+  return React.createElement("div", { id: "algo-importante" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Fortunato",
+      animal: "Tortuga",
+      breed: "Terrestre",
+    }),
+    React.createElement(Pet, {
+      name: "Alaska",
+      animal: "Perro",
+      breed: "Mixto",
+    }),
+    React.createElement(Pet, {
+      name: "Crackovia",
+      animal: "Perro",
+      breed: "",
+    }),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
