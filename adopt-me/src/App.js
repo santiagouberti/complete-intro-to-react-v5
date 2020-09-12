d04c2289db1db528271fdd3c6ea115d035e676ce
Santiago <santiagouberti@gmail.com>
2020-09-12 07:56:44 -0300

    Configuracion para usar JSX

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -3,24 +3,33 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "algo-importante" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Fortunato",
-      animal: "Tortuga",
-      breed: "Terrestre",
-    }),
-    React.createElement(Pet, {
-      name: "Alaska",
-      animal: "Perro",
-      breed: "Mixto",
-    }),
-    React.createElement(Pet, {
-      name: "Crackovia",
-      animal: "Perro",
-      breed: "",
-    }),
-  ]);
+  // return React.createElement("div", { id: "algo-importante" }, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Fortunato",
+  //     animal: "Tortuga",
+  //     breed: "Terrestre",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Alaska",
+  //     animal: "Perro",
+  //     breed: "Mixto",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Crackovia",
+  //     animal: "Perro",
+  //     breed: "Dálmata",
+  //   }),
+  // ]);
+
+  return (
+    <div>
+      <h1 id="algo-importante">Adopt Me!</h1>
+      <Pet name="Fortunato" animal="Tortug" breed="Terrestre" />
+      <Pet name="Alaska" animal="Dog" breed="Mixto" />
+      <Pet name="Crackovia" animal="Dog" breed="Dálmata" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
