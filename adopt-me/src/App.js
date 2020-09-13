13f0b09f81860e7d96b659863c6ac80a7efb48d8
Santiago <santiagouberti@gmail.com>
2020-09-13 20:24:14 -0300

    Hooks y Custom hooks

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,33 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", { id: "algo-importante" }, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Fortunato",
-  //     animal: "Tortuga",
-  //     breed: "Terrestre",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Alaska",
-  //     animal: "Perro",
-  //     breed: "Mixto",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Crackovia",
-  //     animal: "Perro",
-  //     breed: "Dálmata",
-  //   }),
-  // ]);
-
   return (
     <div>
       <h1 id="algo-importante">Adopt Me!</h1>
-      <Pet name="Fortunato" animal="Tortug" breed="Terrestre" />
-      <Pet name="Alaska" animal="Dog" breed="Mixto" />
-      <Pet name="Crackovia" animal="Dog" breed="Dálmata" />
+      <SearchParams />
     </div>
   );
 };
